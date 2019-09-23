b8d337a7b388916e4c5cba2fff16d0ffa29e9d6e
Mr-Recalculate <peregrineautumn@gmail.com>
2019-09-23 12:16:01 -0700

    fixed eq tests and added a few tests for eq

--- location_tests.py
+++ location_tests.py
@@ -7,13 +7,15 @@ class TestLab1(unittest.TestCase):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
     
-    #def test_eq(self):
-    #    loc1 = Location("SLO", 35.3, -120.7)
-    #    loc2 = Location("Paris", 48.9, 2.4)
-    #    self.assertFalse(eq(loc1, loc2))
+    def test_eq_False(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("Paris", 48.9, 2.4)
+        self.assertFalse(loc1 == loc2)
     
-    
-    # Add more tests!
+    def test_eq_True(self):
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("SLO", 35.3, -120.7)
+        self.assertTrue(loc1 == loc2)
 
 if __name__ == "__main__":
         unittest.main()
