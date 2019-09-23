f6b782f448e387757c8ade079c78067de633f3e8
Mr-Recalculate <peregrineautumn@gmail.com>
2019-09-23 10:38:15 -0700

    first commit, worked on location and lab1

--- lab1.py
+++ lab1.py
@@ -1,8 +1,17 @@
-
-def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
+"""finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+def max_list_iter(int_list):  # must use iteration not recursion
+    if int_list == []:
+        raise ValueError('list is empty')
+        return None
+    else:
+        temp = -1
+        for x in int_list:
+            if x > temp:
+                temp = x
+            return temp
+
+
 
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
--- location_tests.py
+++ location_tests.py
@@ -7,6 +7,12 @@ class TestLab1(unittest.TestCase):
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
     
+    #def test_eq(self):
+    #    loc1 = Location("SLO", 35.3, -120.7)
+    #    loc2 = Location("Paris", 48.9, 2.4)
+    #    self.assertFalse(eq(loc1, loc2))
+    
+    
     # Add more tests!
 
 if __name__ == "__main__":
