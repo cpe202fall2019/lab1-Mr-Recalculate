23db6f0acf086302cb925dc0aaf8e383f6fb92c0
Mr-Recalculate <peregrineautumn@gmail.com>
2019-09-23 12:11:08 -0700

    fixed __eq__ method

--- location.py
+++ location.py
@@ -2,12 +2,15 @@
 
 # represents a location using name, latitude and longitude
 class Location:
+
     def __init__(self, name, lat, lon):
         self.name = name    # string for name of location
         self.lat = lat      # latitude in degrees (-90 to 90)
         self.lon = lon      # longitude in degrees (-180 to 180)
+
     def __eq__(self, other):
         return self.name == other.name and self.lat == other.lat and self.lon == other.lon
+    
     def __repr__(self):
         return "Location('%s', %.1f, %.1f)" % (self.name, self.lat, self.lon)
 
