17c2a899f43c6cd4aab262cfff05c2751414b29d
Mr-Recalculate <peregrineautumn@gmail.com>
2019-09-27 16:48:46 -0700

    finished lab1

--- lab1.py
+++ lab1.py
@@ -3,7 +3,6 @@
 def max_list_iter(int_list):  # must use iteration not recursion
     if not int_list:
         raise ValueError('list is empty') 
-
     temp = int_list[0]
     for x in range(len(int_list)):
         if int_list[x] > temp:
@@ -11,13 +10,43 @@ def max_list_iter(int_list):  # must use iteration not recursion
     return temp
 
 
-
-def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
+# must use recursion
+"""recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   pass
+def reverse_rec(int_list, idx=0):   
+   if not int_list:
+       raise ValueError('list is empty') 
+   watch = len(int_list)//2
+   if idx >= len(int_list)//2:
+       return int_list
+   #swaps values in opposite ends of list 
+   temp = int_list[len(int_list)-1-idx]
+   int_list[len(int_list)-1-idx] = int_list[idx]
+   int_list[idx] = temp
+   #increments idx
+   idx = idx+1
+   #recursivly called reverse_rec to swap next pair of values
+   #which are incremental one index closer together
+   reverse_rec(int_list, idx)
+   return int_list
 
-def bin_search(target, low, high, int_list):  # must use recursion
+# must use recursion
    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
-   pass
+def bin_search(target, low, high, int_list):  
+   if not int_list:
+       raise ValueError('list is empty')
+   idx = (low+high)//2
+   if int_list[idx] == target:
+       return idx
+   elif int_list[high] == target:
+      return high
+   elif low+1 == high:
+       return None
+   #if target is bigger than value at idx
+   elif int_list[idx] < target:
+       return bin_search(target, idx, high, int_list)
+    #if target is smaller than value at idx
+   elif int_list[idx] > target:
+       return bin_search(target, low, idx, int_list)
+       
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -19,8 +19,6 @@ class TestLab1(unittest.TestCase):
         """tests for max value when ints in list are sorted
         in ascending order"""
         tlist = [-9999,1,2,3,4,5,6,7,8,9,9999]
-        print ("a-list " + str(tlist))
-        print ("a-max Value " + str(max_list_iter(tlist)))
         self.assertEqual(max_list_iter(tlist), 9999)
 
     def test_max_list_iter_sorted_list_decending(self):
@@ -29,14 +27,81 @@ class TestLab1(unittest.TestCase):
         tlist = [9999,9,8,7,6,5,4,3,2,1,-9999]
         self.assertEqual(max_list_iter(tlist), 9999)
 
-    #def test_reverse_rec(self):
-    #    self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+    def test_reverse_rec_empty(self):
+        """tests if recursive function raises ValueError if empty list is passed in"""
+        tlist = None
+        with self.assertRaises(ValueError):  # used to check for exception
+            reverse_rec(tlist)
 
-    #def test_bin_search(self):
-    #    list_val =[0,1,2,3,4,7,8,9,10]
-    #    low = 0
-    #    high = len(list_val)-1
-    #    self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+    def test_reverse_rec(self):
+        self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+
+    def test_reverse_rec_zeros(self):
+        self.assertEqual(reverse_rec([0,0,0,0,0,0,0]),[0,0,0,0,0,0,0])
+
+    def test_reverse_rec_gross(self):
+        self.assertEqual(reverse_rec([-4,5,-7,-9,3,1,2,-100]),[-100,2,1,3,-9,-7,5,-4])
+
+    def test_bin_search_empty(self):
+        """tests if recursive function raises ValueError if empty list is passed in"""
+        tlist = None
+        with self.assertRaises(ValueError):  # used to check for exception
+            bin_search(0, 0, 0, tlist)
+
+    def test_bin_search_no_target(self):
+        #does bin_search return None if it fails to find target?
+        list_val =[0,1,2,3,5,7,8,9,10]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(4, low, high, list_val), None)
+
+    def test_bin_search_beginning_even(self):
+        #does bin_search return the index of a target at the beginning of a list
+        #with an even amount of entries?
+        list_val =[0,1,2,3,5,7,8,9,10,11]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(0, low, high, list_val), 0)
+
+    def test_bin_search_beginning_odd(self):
+        #does bin_search return the index of a target at the beginning of a list
+        #with an odd amount of entries?
+        list_val =[0,1,2,3,5,7,8,9,10]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(0, low, high, list_val), 0)
+
+    def test_bin_search_end_even(self):
+        #does bin_search return the index of a target at the end of a list
+        #with an even amount of entries?
+        list_val =[0,1,2,3,5,7,8,9,10,11]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(10, low, high, list_val), 8)
+
+    def test_bin_search_end_odd(self):
+        #does bin_search return the index of a target at the end of a list
+        #with an odd amount of entries?
+        list_val =[0,1,2,3,5,7,8,9,10]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(10, low, high, list_val), 8)
+
+    def test_bin_search_even(self):
+        #does bin_search return the index of a target in the middle of a list
+        #with an even amount of entries?
+        list_val =[0,1,2,3,4,7,8,9,10,11]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(4, low, high, list_val), 4)
+
+    def test_bin_search_odd(self):
+        #does bin_search return the index of a target in the middle of a list
+        #with an odd amount of entries?
+        list_val =[0,1,2,3,4,7,8,9,10]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(4, low, high, list_val), 4)
 
 if __name__ == "__main__":
         unittest.main()
